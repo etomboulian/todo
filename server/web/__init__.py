85a0d847e5735db274424233a0dba33c79e50dee
Evan Tomboulian <etomboulian@gmail.com>
2024-06-02 21:30:54 -0700

    fix up some things

--- server/web/__init__.py
+++ server/web/__init__.py
@@ -1,8 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 from .customer import bp as cust_bp
 from .todos import bp as todos_bp
 
 bp = Blueprint("web", __name__)
 bp.register_blueprint(cust_bp)
 bp.register_blueprint(todos_bp)
+
+@bp.route("/", methods=["GET"])
+def home():
+    return render_template("home.html")
 
--- server/web/customer.py
+++ server/web/customer.py
@@ -1,7 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint("customer", __name__, url_prefix='/customers/')
 
 @bp.route('/')
 def customer_list():
-    ...
+    return render_template("customers.html")
